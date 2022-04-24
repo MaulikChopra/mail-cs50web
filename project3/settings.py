467125a154df973f0e662a7ccaff51104f6fdcfc
maulik <maulikchopra10@gmail.com>
2022-04-24 21:29:00 +0530

    stativ

--- project3/settings.py
+++ project3/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = '/static/'
 # location where django collect all static files
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # location where you will store your static files
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'mail/static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')
                     ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
