81fcd4574a4be83837910afd15c9718952e09161
maulik <maulikchopra10@gmail.com>
2022-04-24 22:23:11 +0530

    changes

--- project3/settings.py
+++ project3/settings.py
@@ -129,13 +129,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+STATIC_URL = '/static/'  # this is only for the html syntax
 
-STATIC_URL = '/static/'
 # location where django collect all static files
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # location where you will store your static files
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'mail/static')
-                    ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'mail/static')]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
