d7318bd15011103f7c569c5860c0fedddc0f2570
maulik <maulikchopra10@gmail.com>
2022-04-24 21:26:22 +0530

    update for server error 500

--- project3/settings.py
+++ project3/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # location where django collect all static files
-STATIC_ROOT = os.path.join(BASE_DIR, '/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # location where you will store your static files
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'mail/static')
                     ]
